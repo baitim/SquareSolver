838f97dedfd292c64abc6bdc7f9a19cc6648be5d
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2023-08-17 16:23:35 +0300

    учёл больше случаев, добавил проверку ввода

--- SquareSolver_main.cpp
+++ SquareSolver_main.cpp
@@ -4,50 +4,75 @@
 #include <math.h>
 #define eps 1e-6
 
-int sol(double a, double b, double c);
+void input_coefficients(double* a, double* b, double* c);
+
+int sol(double a, double b, double c, double* D);
+
+bool is_double_equal(double x, double y);
 
 int main (void) {
     double a = 0, b = 0, c = 0;
     printf ("Enter the coefficients for the quadratic equation a*x^2 + b*x + c = 0 separated by a space\n");
+
+    input_coefficients(&a, &b, &c);
+
+    double D;
+    int roots = sol(a, b, c, &D);
+    if (roots >= 0 and roots <= 2)
+        printf ("Total roots: %d\n", roots);
+    double x, x1, x2;
+    double Dsqrt = sqrt(D);
+
+    switch (roots) {
+        case 0: // 0 корней, уравнение квадратное или a = 0, b = 0, c != 0
+            break;
+        case 1: // 1 корень, уравнение квадратное
+            x = (-b+Dsqrt)/2/a;
+            printf ("x = %lf\n", x);
+            break;
+        case 2: // 2 корня, уравнение квадратное
+            x1 = (-b-Dsqrt)/2/a;
+            x2 = (-b+Dsqrt)/2/a;
+            printf ("x1 = %lf  x2 = %lf\n", x1, x2);
+            break;
+        case 3: // 1 корень, уравнение линейное
+            printf ("The equation is not square, the root = %lf\n", -c/b);
+        case 4: // бесконенчно много корней
+            printf ("The equation is not square, infinitely many roots");
+    }
+    return 0;
+}
+
+void input_coefficients(double* a, double* b, double* c) {
     while (true) {
-        int count_input = scanf ("%lf%lf%lf", &a, &b, &c);
+        int count_input = scanf ("%lf%lf%lf", a, b, c);
         if (count_input != 3 or count_input == EOF) {
             printf ("incorrect input, input: 0 - to exit, another integer - to continue\n");
             while (getchar() != '\n');
             int x;
             scanf ("%d", &x);
-            if (x == 0) return 0;
+            if (x == 0) exit(0);
         }
         else
             break;
     }
-    int roots = sol(a, b, c);
-    if (roots >= 0)
-        printf ("Total roots: %d\n", roots);
-    double x = 0, x1 = 0, x2 = 0;
-    double D = b*b - 4*a*c, Dsqrt = sqrt(D);
-    switch (roots) {
-        case 0:
-            break;
-        case 1:
-            x = (-b+Dsqrt)/2/a;
-            printf ("x = %lf\n", x);
-            break;
-        case 2:
-            x1 = (-b-Dsqrt)/2/a;
-            x2 = (-b+Dsqrt)/2/a;
-            printf ("x1 = %lf  x2 = %lf\n", x1, x2);
-            break;
-        default:
-            printf ("The equation is not square, the root = %lf\n", -c/b);
-    }
-    return 0;
 }
 
-int sol(double a, double b, double c) {
-    if (a >= -eps and a <= eps) return -1;
-    double D = b*b - 4*a*c;
-    if (D < 0) return 0;
-    if (D >= -eps and D <= eps) return 1;
-    if (D > 0) return 2;
+int sol(double a, double b, double c, double* D) {
+    if (is_double_equal(a, 0)) {
+        if (b != 0) return 3;
+        if (b == 0) {
+            if (c == 0) return 4;
+            if (c != 0) return 0;
+        }
+    }
+    *D = b*b - 4*a*c;
+    if (*D < 0) return 0;
+    if (is_double_equal(*D, 0)) return 1;
+    if (*D > 0) return 2;
+}
+
+bool is_double_equal(double x, double y) {
+    if (abs(x-y) <= eps) return true;
+    else return false;
 }
