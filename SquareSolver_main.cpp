6d483e36402e9e15a779a1d93101b5d9f4593d6b
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2023-08-17 15:49:28 +0300

    Add files via upload

--- SquareSolver_main.cpp
+++ SquareSolver_main.cpp
@@ -4,38 +4,50 @@
 #include <math.h>
 #define eps 1e-6
 
-double D;
+int sol(double a, double b, double c);
 
-int sol(double a, double b, double c) {
-    if (a == 0) return -1;
-    D = b*b - 4*a*c;
-    if (D < 0) return 0;
-    if (D >= -eps and D <= eps) return 1;
-    if (D > 0) return 2;
-}
-
-int main () {
-    double a, b, c;
+int main (void) {
+    double a = 0, b = 0, c = 0;
     printf ("Enter the coefficients for the quadratic equation a*x^2 + b*x + c = 0 separated by a space\n");
-    scanf ("%lf%lf%lf", &a, &b, &c);
+    while (true) {
+        int count_input = scanf ("%lf%lf%lf", &a, &b, &c);
+        if (count_input != 3 or count_input == EOF) {
+            printf ("incorrect input, input: 0 - to exit, another integer - to continue\n");
+            while (getchar() != '\n');
+            int x;
+            scanf ("%d", &x);
+            if (x == 0) return 0;
+        }
+        else
+            break;
+    }
     int roots = sol(a, b, c);
     if (roots >= 0)
         printf ("Total roots: %d\n", roots);
-    double x, x1, x2;
+    double x = 0, x1 = 0, x2 = 0;
+    double D = b*b - 4*a*c, Dsqrt = sqrt(D);
     switch (roots) {
         case 0:
             break;
         case 1:
-            x = (-b+sqrt(D))/2/a;
-            printf ("x = %lf", x);
+            x = (-b+Dsqrt)/2/a;
+            printf ("x = %lf\n", x);
             break;
         case 2:
-            x1 = (-b-sqrt(D))/2/a;
-            x2 = (-b+sqrt(D))/2/a;
-            printf ("x1 = %lf  x2 = %lf", x1, x2);
+            x1 = (-b-Dsqrt)/2/a;
+            x2 = (-b+Dsqrt)/2/a;
+            printf ("x1 = %lf  x2 = %lf\n", x1, x2);
             break;
         default:
-            printf ("The equation is not square, the root = %lf", -c/b);
+            printf ("The equation is not square, the root = %lf\n", -c/b);
     }
     return 0;
+}
+
+int sol(double a, double b, double c) {
+    if (a >= -eps and a <= eps) return -1;
+    double D = b*b - 4*a*c;
+    if (D < 0) return 0;
+    if (D >= -eps and D <= eps) return 1;
+    if (D > 0) return 2;
 }
