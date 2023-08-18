35d0b0a601ca702dd1a5e95da366891aa8e995ca
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2023-08-18 15:45:41 +0300

    Add files via upload
    updated codestyle

--- SquareSolver_main.cpp
+++ SquareSolver_main.cpp
@@ -5,92 +5,125 @@
 #include <ctype.h>
 #define eps 1e-6
 
-enum cases { root_0, root_1_quad, roots_2_quad, root_1_line, root_inf };
+struct coefs {
+        double a = 0;
+        double b = 0;// coefficients
+        double c = 0;
+};
+
+enum cases { root_0, root_1_quad, roots_2_quad, root_1_line, root_inf, err };
 
 void print_roots(double x1, double x2, cases roots);
 
-void input_coefficients(double* a, double* b, double* c);
+void input_coefficients(coefs *coef);
 
 bool check_input(int count_input);
 
-cases calculation_of_roots(double a, double b, double c, double* x1, double* x2);
+cases calculation_of_roots(coefs coef, double *x1, double *x2);
 
 bool is_double_equal(double x, double y);
 
-int main (void) {
-    double a = 0, b = 0, c = 0; // coefficients
-    printf ("Enter the coefficients for the quadratic equation a*x^2 + b*x + c = 0 separated by a space\n");
+int main (void)
+{
+        coefs coef;
+        printf ("Enter the coefficients for the quadratic equation a*x^2 + b*x + c = 0 separated by a space\n");
 
-    input_coefficients(&a, &b, &c); // input
+        input_coefficients(&coef); // input
 
-    double x1 = 0, x2 = 0; // roots
-    cases roots;
-    roots = calculation_of_roots(a, b, c, &x1, &x2);
-    print_roots(x1, x2, roots); // answer
-    return 0;
+        double x1 = 0, x2 = 0; // roots
+        cases roots = root_0;
+        roots = calculation_of_roots(coef, &x1, &x2);
+        print_roots(x1, x2, roots); // answer
+        return 0;
 }
 
-void print_roots(double x1, double x2, cases roots) {
-    switch (roots) {
+void print_roots(double x1, double x2, cases roots)
+{
+        switch (roots) {
         case root_0: // 0 roots, the equation is square or a = 0, b = 0, c != 0
-            break;
+                printf ("Total roots: 0");
+                break;
         case root_1_quad: // 1 root, quadratic equation
-            printf ("Total roots: 1\nx = %lf\n", x1);
-            break;
+                printf ("Total roots: 1\nx = %lf\n", x1);
+                break;
         case roots_2_quad: // 2 roots, the equation is square
-            printf ("Total roots: 2\nx1 = %lf  x2 = %lf\n", x1, x2);
-            break;
+                printf ("Total roots: 2\nx1 = %lf  x2 = %lf\n", x1, x2);
+                break;
         case root_1_line: // 1 root, linear equation
-            printf ("The equation is not square, the root = %lf\n", x1);
+                printf ("The equation is not square, the root = %lf\n", x1);
+                break;
         case root_inf: // infinitely many roots
-            printf ("The equation is not square, infinitely many roots");
-    }
-}
-
-void input_coefficients(double* a, double* b, double* c) {
-    while (true) {
-        int count_input = scanf ("%lf%lf%lf", a, b, c);
-        bool f = check_input(count_input);
-        if (f) break;
-    }
-}
-
-bool check_input(int count_input) {
-    bool rest_of_input = false;
-    char c;
-    while ((c = getchar()) != '\n')
-        if (c != ' ' and c != '\t')
-            rest_of_input = true; // check rest of input
-
-    if (rest_of_input or count_input != 3 or count_input == EOF) {
-        printf ("incorrect input, input: 0 - to exit, another integer - to continue\n");
-        int x;
-        while (getchar() != '\n');
-        scanf ("%d", &x);
-        while (getchar() != '\n');
-        if (x == 0) exit(0); // exit
-        return false; // continue input
-    }
-    else
-        return true;
-}
-
-cases calculation_of_roots(double a, double b, double c, double* x1, double* x2) {
-    if (is_double_equal(a, 0)) {
-        if (b != 0) { *x1 = -c/b; return root_1_line; }
-        if (b == 0) {
-            if (c == 0) return root_inf;
-            if (c != 0) return root_0;
+                printf ("The equation is not square, infinitely many roots");
+                break;
+        default:
+                printf ("ERROR");
+                break;
         }
-    }
-    double D = b*b - 4*a*c;
-    double Dsqrt = sqrt(D);
-    if (D < 0) return root_0;
-    if (is_double_equal(D, 0)) { *x1 = -b/2/a; return root_1_quad; }
-    if (D > 0) { *x1 = (-b-Dsqrt)/2/a; *x2 = (-b+Dsqrt)/2/a; return roots_2_quad; }
 }
 
-bool is_double_equal(double x, double y) {
-    if (abs(x-y) <= eps) return true;
-    else return false;
+void input_coefficients(coefs *coef)
+{
+        while (true) {
+                int count_input = scanf ("%lf%lf%lf", &coef->a, &coef->b, &coef->c);
+                if (check_input(count_input))
+                        break;
+        }
+}
+
+bool check_input(int count_input)
+{
+        bool rest_of_input = false;
+        char c = '$';
+        while ((c = (char)getchar()) != '\n')
+                if (c != ' ' and c != '\t')
+                        rest_of_input = true; // check rest of input
+
+        if (rest_of_input or count_input != 3 or count_input == EOF) {
+                printf ("incorrect input, input: 0 - to exit, another integer - to continue\n");
+                int x = 1;
+                scanf ("%d", &x);
+                while (getchar() != '\n')
+                        ;
+                if (x == 0)
+                        exit(0); // exit
+                return false; // continue input
+        } else
+                return true; // correct input
+
+}
+
+cases calculation_of_roots(coefs coef, double *x1, double *x2)
+{
+        if (is_double_equal(coef.a, 0)) { // equation is not square a = 0
+                if (!is_double_equal(coef.b, 0)) { // b = 0
+                        *x1 = -coef.c / coef.b;// linear equation  a = 0, x = -c/b
+                        return root_1_line;
+                } else {                           // b != 0
+                        if (is_double_equal(coef.c, 0)) // infinitely many roots a = b = c = 0
+                                return root_inf;
+                        else
+                                return root_0; // 0 roots a = b = 0, c != 0
+                }
+        }
+
+        double D = coef.b * coef.b - 4 * coef.a * coef.c; // equation is square a != 0
+        if (D < 0)
+                return root_0; // 0 roots D < 0
+
+        double Dsqrt = sqrt(D); // for optimization
+        if (is_double_equal(D, 0)) {
+                *x1 = -coef.b / 2 / coef.a;
+                return root_1_quad; // 1 root D == 0
+        }
+        if (D > 0) {
+                *x1 = (-coef.b - Dsqrt) / 2 / coef.a;
+                *x2 = (-coef.b + Dsqrt) / 2 / coef.a;
+                return roots_2_quad; // 2 roots D > 0
+        }
+        return err; // default
+}
+
+bool is_double_equal(double x, double y)
+{
+        return ((y - x) >= -eps and (x - y) <= eps);
 }
